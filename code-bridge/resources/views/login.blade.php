b3c8ed8da49bdd3e0eccbf79216f4d7a6a8e8599
Muhammad Bilal <info.bilalshakeel@gmail.com>
2024-04-28 23:11:34 +0500

    Add new routes for account, jobs-applied, job-detail, jobs, login, my-jobs, post-job, register, and saved-job

--- code-bridge/resources/views/login.blade.php
+++ code-bridge/resources/views/login.blade.php
@@ -2,163 +2,34 @@
 
 @section('main-section')
 
-<section class="section-5 bg-2">
-    <div class="container py-5">
-        <div class="row">
-            <div class="col">
-                <nav aria-label="breadcrumb" class=" rounded-3 p-3 mb-4">
-                    <ol class="breadcrumb mb-0">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Saved Jobs</li>
-                    </ol>
-                </nav>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-3">
-                <div class="card border-0 shadow mb-4 p-3">
-                    <div class="s-body text-center mt-3">
-                        <img src="assets/images/avatar7.png" alt="avatar"  class="rounded-circle img-fluid" style="width: 150px;">
-                        <h5 class="mt-3 pb-0">Mohit Singh</h5>
-                        <p class="text-muted mb-1 fs-6">Full Stack Developer</p>
-                        <div class="d-flex justify-content-center mb-2">
-                            <button data-bs-toggle="modal" data-bs-target="#exampleModal" type="button" class="btn btn-primary">Change Profile Picture</button>
+<section class="section-5">
+    <div class="container my-5">
+        <div class="py-lg-2">&nbsp;</div>
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-5">
+                <div class="card shadow border-0 p-5">
+                    <h1 class="h3">Login</h1>
+                    <form action="account.html" method="post">
+                        <div class="mb-3">
+                            <label for="" class="mb-2">Email*</label>
+                            <input type="text" name="email" id="email" class="form-control" placeholder="example@example.com">
+                        </div> 
+                        <div class="mb-3">
+                            <label for="" class="mb-2">Password*</label>
+                            <input type="password" name="name" id="name" class="form-control" placeholder="Enter Password">
+                        </div> 
+                        <div class="justify-content-between d-flex">
+                        <button class="btn btn-primary mt-2">Login</button>
+                            <a href="forgot-password.html" class="mt-3">Forgot Password?</a>
                         </div>
-                    </div>
+                    </form>                    
                 </div>
-                <div class="card account-nav border-0 shadow mb-4 mb-lg-0">
-                    <div class="card-body p-0">
-                        <ul class="list-group list-group-flush ">
-                            <li class="list-group-item d-flex justify-content-between p-3">
-                                <a href="account.html">Account Settings</a>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                                <a href="post-job.html">Post a Job</a>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                                <a href="my-jobs.html">My Jobs</a>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                                <a href="job-applied.html">Jobs Applied</a>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                                <a href="saved-jobs.html">Saved Jobs</a>
-                            </li>                                                        
-                        </ul>
-                    </div>
+                <div class="mt-4 text-center">
+                    <p>Do not have an account? <a  href="register.html">Register</a></p>
                 </div>
             </div>
-            <div class="col-lg-9">
-                <div class="card border-0 shadow mb-4 p-3">
-                    <div class="card-body card-form">
-                        <h3 class="fs-4 mb-1">Saved Jobs</h3>
-                        <div class="table-responsive">
-                            <table class="table ">
-                                <thead class="bg-light">
-                                    <tr>
-                                        <th scope="col">Title</th>
-                                        <th scope="col">Job Created</th>
-                                        <th scope="col">Applicants</th>
-                                        <th scope="col">Status</th>
-                                        <th scope="col">Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody class="border-0">
-                                    <tr class="active">
-                                        <td>
-                                            <div class="job-name fw-500">Web Developer</div>
-                                            <div class="info1">Fulltime . Noida</div>
-                                        </td>
-                                        <td>05 Jun, 2023</td>
-                                        <td>130 Applications</td>
-                                        <td>
-                                            <div class="job-status text-capitalize">active</div>
-                                        </td>
-                                        <td>
-                                            <div class="action-dots float-end">
-                                                <a href="#" class="" data-bs-toggle="dropdown" aria-expanded="false">
-                                                    <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
-                                                </a>
-                                                <ul class="dropdown-menu dropdown-menu-end">
-                                                    <li><a class="dropdown-item" href="job-detail.html"> <i class="fa fa-eye" aria-hidden="true"></i> View</a></li>
-                                                    <li><a class="dropdown-item" href="#"><i class="fa fa-trash" aria-hidden="true"></i> Remove</a></li>
-                                                </ul>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr class="pending">
-                                        <td>
-                                            <div class="job-name fw-500".html Developer</div>
-                                            <div class="info1">Part-time . Delhi</div>
-                                        </td>
-                                        <td>13 Aug, 2023</td>
-                                        <td>20 Applications</td>
-                                        <td>
-                                            <div class="job-status text-capitalize">pending</div>
-                                        </td>
-                                        <td>
-                                            <div class="action-dots float-end">
-                                                <a href="#" class="" data-bs-toggle="dropdown" aria-expanded="false">
-                                                    <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
-                                                </a>
-                                                <ul class="dropdown-menu dropdown-menu-end">
-                                                    <li><a class="dropdown-item" href="job-detail.html"> <i class="fa fa-eye" aria-hidden="true"></i> View</a></li>
-                                                    <li><a class="dropdown-item" href="#"><i class="fa fa-trash" aria-hidden="true"></i> Remove</a></li>
-                                                </ul>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr class="expired">
-                                        <td>
-                                            <div class="job-name fw-500">Full Stack Developer</div>
-                                            <div class="info1">Fulltime . Noida</div>
-                                        </td>
-                                        <td>27 Sep, 2023</td>
-                                        <td>278 Applications</td>
-                                        <td>
-                                            <div class="job-status text-capitalize">expired</div>
-                                        </td>
-                                        <td>
-                                            <div class="action-dots float-end">
-                                                <a href="#" class="" data-bs-toggle="dropdown" aria-expanded="false">
-                                                    <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
-                                                </a>
-                                                <ul class="dropdown-menu dropdown-menu-end">
-                                                    <li><a class="dropdown-item" href="job-detail.html"> <i class="fa fa-eye" aria-hidden="true"></i> View</a></li>
-                                                    <li><a class="dropdown-item" href="#"><i class="fa fa-trash" aria-hidden="true"></i> Remove</a></li>
-                                                </ul>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr class="active">
-                                        <td>
-                                            <div class="job-name fw-500">Developer for IT company</div>
-                                            <div class="info1">Fulltime . Goa</div>
-                                        </td>
-                                        <td>14 Feb, 2023</td>
-                                        <td>70 Applications</td>
-                                        <td>
-                                            <div class="job-status text-capitalize">active</div>
-                                        </td>
-                                        <td>
-                                            <div class="action-dots float-end">
-                                                <a href="#" class="" data-bs-toggle="dropdown" aria-expanded="false">
-                                                    <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
-                                                </a>
-                                                <ul class="dropdown-menu dropdown-menu-end">
-                                                    <li><a class="dropdown-item" href="job-detail.html"> <i class="fa fa-eye" aria-hidden="true"></i> View</a></li>
-                                                    <li><a class="dropdown-item" href="#"><i class="fa fa-trash" aria-hidden="true"></i> Remove</a></li>
-                                                </ul>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div> 
-            </div>
         </div>
+        <div class="py-lg-5">&nbsp;</div>
     </div>
 </section>
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
--- code-bridge/routes/web.php
+++ code-bridge/routes/web.php
@@ -1,10 +1,46 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/account', function () {
+    return view('account');
+});
+
+Route::get('/jobs-applied', function () {
+    return view('job-applied');
+});
+
+Route::get('/job-detail', function () {
+    return view('job-detail');
+});
+
+Route::get('/jobs', function () {
+    return view('jobs');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/my-jobs', function () {
+    return view('my-jobs');
+});
+
+Route::get('/post-job', function () {
+    return view('post-job');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/saved-job', function () {
+    return view('saved-job');
+});
+
